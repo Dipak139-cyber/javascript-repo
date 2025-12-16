08df10ccd6fb62c805a972a2e8b7ec2e1d97cc3f
Dipak Ray <dy0348086@gmail.com>
2025-12-16 16:01:13 +0545

    code are committed

---
+++ function/numupto100divby5.js
@@ -0,0 +1,10 @@
+function numupto100divby5(n){
+
+    for(i=1;i<=100;i++){
+        if(i%5==0){
+
+            console.log(i)
+        }
+    }
+}
+numupto100divby5(100);
---
+++ function/sumofoddnumupto50.js
@@ -0,0 +1,10 @@
+function sumoddnupto50(n){
+    let sum=0;
+    for(i=1;i<=50;i++){
+        if(i%2!==0){
+           sum=sum+i;
+        }
+    }
+    console.log(sum)
+}
+sumoddnupto50(50);
